9e0ff5dbc79995aa0defb4164b0a8e7a7dad260d
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2024-01-10 16:06:54 +0530

    Add Guess struct and implement Guess::new() and Guess::value() methods

--- Rust/src/library.rs
+++ Rust/src/library.rs
@@ -8,6 +8,21 @@ pub fn guessing_game() {
     use rand::Rng;
     use std::cmp::Ordering;
     use std::io;
+
+    struct Guess {
+        value: i32,
+    }
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}.", value);
+            }
+            Guess { value }
+        }
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+    }
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -21,21 +36,21 @@ pub fn guessing_game() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        println!("You guessed: {}", &guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => Guess::new(guess).value(),
+            Ordering::Greater => Guess::new(guess).value(),
             Ordering::Equal => {
                 println!("You win!");
                 break;
             }
-        }
+        };
     }
 }
 
