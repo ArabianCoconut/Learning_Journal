b17417a624c32635e4b897ec8e883288c553c1bd
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2024-01-10 16:07:39 +0530

    Update program text formatting

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -9,16 +9,16 @@ fn main() {
     let program_text = indoc! {"
     Author: ArabianCoconut
     Description: Basic CLI program for practice from The Rust book.\n
-      Please choose an option:
-      1. Guessing Game
-      2. Temperature Converting
-      3. Fibonacci
-      4. Twelve Days of Christmas
-      5. Rectangle Area
-      6. Employee Data
-      7. Median and Mode
-      8. Pig Latin\n
-      Note: Type q to quit
+    Please choose an option:
+    1. Guessing Game
+    2. Temperature Converting
+    3. Fibonacci
+    4. Twelve Days of Christmas
+    5. Rectangle Area
+    6. Employee Data
+    7. Median and Mode
+    8. Pig Latin\n
+    Note: Type q to quit
     "};
     println!("{}", program_text);
     let example = vec![1, 2, 3, 4, 3];
