00ab8a23c8b2621a5b1f7aee5c9083629431294e
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2024-01-10 16:37:43 +0530

    Refactor Guess struct visibility and methods

--- Rust/src/library.rs
+++ Rust/src/library.rs
@@ -13,13 +13,13 @@ pub fn guessing_game() {
         value: i32,
     }
     impl Guess {
-        pub fn new(value: i32) -> Guess {
+        fn new(value: i32) -> Guess {
             if value < 1 || value > 100 {
                 panic!("Guess value must be between 1 and 100, got {}.", value);
             }
             Guess { value }
         }
-        pub fn value(&self) -> i32 {
+        fn value(&self) -> i32 {
             self.value
         }
     }
